94327d5f2b74506147b1cc4d001eb8c01be19b03
Gabrielscruz <54606517+Gabrielscruz@users.noreply.github.com>
2022-10-27 23:48:14 -0300

    ajuste

---
+++ src/components/Button.tsx
@@ -0,0 +1,23 @@
+import { Icon } from './Icon';
+
+import '../styles/button.scss';
+import { ButtonHTMLAttributes, memo } from 'react';
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  title: string;
+  iconName: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  selected: boolean;
+}
+ 
+export function ButtonConponent({ iconName, title, selected, ...rest }: ButtonProps) {
+  return (
+    <button type="button" {...(selected && { className: 'selected' })} {...rest}>
+      <Icon name={iconName} color={selected ? '#FAE800' : '#FBFBFB'} />
+      {title}
+    </button>
+  );
+}
+
+export const Button = memo(ButtonConponent, (prevProps, nextProps) => {
+  return prevProps.selected === nextProps.selected
+})
